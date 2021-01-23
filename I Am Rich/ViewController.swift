6e50c60b528e181774d52630e84a42d47263de4a
ravi2612 <ravinavarro26@gmail.com>
2021-01-22 23:40:25 -0300

    final

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by raji navarro on 20/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
